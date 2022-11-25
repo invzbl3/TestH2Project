73e89d683614e4d06c08a6c2119a842a4dd94fc0
Maksym Rohachov <romax982@gmail.com>
2022-11-25 15:59:47 +0100

    Update application.properties, sql scripts

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,7 +3,7 @@
 /*==============================================================*/
 create table items
 (
-    item_id               int not null auto_increment,
+    id                    int not null auto_increment,
     designation           varchar(50) not null,
-    primary key (item_id)
+    primary key (id)
 );
